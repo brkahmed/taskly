012c0babaedda2cc1cc5c102b497b5b463916350
Ahmed <braika7med@gmail.com>
2024-08-17 21:49:10 +0100

    add supported queries

--- task-cli.py
+++ task-cli.py
@@ -29,7 +29,62 @@ supported queries schema:
 
 
 def main() -> None:
-    ...
+    supported_queries = {
+        "add": {
+            "target": add_task,
+            "help": "add task to your list",
+            "args": [{"name_or_flags": ["description"], "help": "task description"}],
+        },
+        "delete": {
+            "target": delete_task,
+            "help": "delete task from your list",
+            "args": [
+                {
+                    "name_or_flags": ["id"],
+                    "help": "id of task you want to delete",
+                    "type": int,
+                }
+            ],
+        },
+        "update": {
+            "target": update_task,
+            "help": "update task description",
+            "args": [
+                {
+                    "name_or_flags": ["id"],
+                    "help": "id of task you want to update",
+                    "type": int,
+                },
+                {
+                    "name_or_flags": ["description"],
+                    "help": "new description for the task",
+                },
+            ],
+        },
+        "list": {
+            "target": list_task,
+            "help": "list all yout task",
+            "args": [
+                {
+                    "name_or_flags": ["--status", "-s"],
+                    "help": "status of task you want to list by default is all",
+                    "choices": ["all", "done", "todo", "in-progress"],
+                    "type": str.lower,
+                    "default": "all",
+                }
+            ],
+        },
+        "mark-in-progress": {
+            "target": mark_in_progress_task,
+            "help": "set status of task to in-progress",
+            "args": [{"name_or_flags": ["id"], "help": "id of task", "type": int}],
+        },
+        "mark-done": {
+            "target": mark_done_task,
+            "help": "set status of task to done",
+            "args": [{"name_or_flags": ["id"], "help": "id of task", "type": int}],
+        },
+    }
 
 
 def load_database() -> dict[str, dict]: ...
@@ -52,7 +107,7 @@ def update_task(database: dict[str, dict], id: int, description: str) -> None: .
 
 def list_task(
     database: dict[str, dict],
-    type: Literal["all", "done", "in-progress", "todo"] = "all",
+    status: Literal["all", "done", "in-progress", "todo"] = "all",
 ) -> None: ...
 
 
